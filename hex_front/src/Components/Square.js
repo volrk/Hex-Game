a356ab5d102b85db67ec130558893459f3c3a571
Audrey <aud.boulard@gmail.com>
2020-04-13 15:06:00 +0200

    Send POST request when user press on a cell

--- hex_front/src/Components/Square.js
+++ hex_front/src/Components/Square.js
@@ -2,9 +2,22 @@ import * as React from 'react';
 import './Square.css';
 
 export default class Square extends React.Component {
+    handleClick() {
+        fetch(`${process.env.REACT_APP_RASPBERRY || ""}/play`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ playeur: 1, x: this.props.idxX, y: this.props.idxY })
+        });
+    }
+
     render() {
         return (
-            <button className="square" />
+            <button className="square" onClick={() => this.handleClick()}>
+                X: {this.props.idxX} - Y: {this.props.idxY}
+            </button >
         );
     }
 }
