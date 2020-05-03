e29df5b84101dbc2eeb53541c9f5ab69c4eddb7c
Audrey <aud.boulard@gmail.com>
2020-05-03 23:57:40 +0200

    Fix 404

--- hex_front/src/NewGame.js
+++ hex_front/src/NewGame.js
@@ -18,13 +18,13 @@ export default function NewGame() {
     })
     .then(res => res.json())
     .then(result => {
-      setGame(result); 
-      setPlayerId(result.player); 
-      setInit(true); 
+      setGame(result);
+      setPlayerId(result.player);
+      setInit(true);
       setCanPlay(true);
       setGameId(result.id);
     }
-  );
+    );
 
   let updateGame = () => fetch(
     `${process.env.REACT_APP_RASPBERRY || ""}/hex/get/${gameId}`,
@@ -35,56 +35,66 @@ export default function NewGame() {
     .then(result => {
       setGame(result);
     }
-  );
+    );
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      await updateGame();
-      setToogleTimer(!toogleTimer);
+      try {
+        if (init && (!game.winner)) {
+          await updateGame();
+        }
+      }
+      finally {
+        setToogleTimer(!toogleTimer);
+      }
     }, 1000);
     return () => { clearTimeout(timer); };
   }, [toogleTimer]);
 
-  let handleClickJoinGame = () => {
-    updateGame();
-    setInit(true); setCanPlay(true);
+  let handleClickJoinGame = async () => {
+    try {
+      await updateGame();
+      setInit(true); setCanPlay(true);
+    }
+    catch (err) {
+    }
   };
 
   if (!init) {
     return (
-    <div>
-      <Popup
-        trigger={<button className="button"> Nouveau jeu</button>}
-        modal
-        closeOnDocumentClick>
+      <div>
+        <Popup
+          trigger={<button className="button"> Nouveau jeu</button>}
+          modal
+          closeOnDocumentClick>
           <div className="modal">
             <div>
               <label>Taille du jeu : </label>
-              <input type="number" id="boardId" min="1" max="15" onChange={event => setBoardSize(event.target.value)}/>
+              <input type="number" id="boardId" min="1" max="15" onChange={event => setBoardSize(event.target.value)} />
             </div>
             <button onClick={() => { handleClickNewGame() }}>Nouveau</button >
           </div>
-      </Popup>
-      <Popup
-        trigger={<button className="button"> Rejoindre un jeu </button>}
-        modal
-        closeOnDocumentClick>
+        </Popup>
+        <Popup
+          trigger={<button className="button"> Rejoindre un jeu </button>}
+          modal
+          closeOnDocumentClick>
           <div className="modal">
             <div>
               <label>Partie : </label>
-              <input type="number" id="boardId" min="0" max="9" onChange={event => setGameId(event.target.value)}/>
+              <input type="number" id="boardId" min="0" max="9" onChange={event => setGameId(event.target.value)} />
             </div>
             <div>
               <label>Joueur : </label>
-              <input type="radio" id="1" name="playeur" checked={playerID === 1} onChange = {() => setPlayerId(1)}/>
+              <input type="radio" id="1" name="playeur" checked={playerID === 1} onChange={() => setPlayerId(1)} />
               <label>1</label>
-              <input type="radio" id="2" name="playeur" checked={playerID === 2} onChange = {() => setPlayerId(2)}/>
+              <input type="radio" id="2" name="playeur" checked={playerID === 2} onChange={() => setPlayerId(2)} />
               <label>2</label>
             </div>
             <button onClick={() => { handleClickJoinGame() }}>Rejoindre</button >
           </div>
-      </Popup>
-    </div>)
+        </Popup>
+      </div>)
   }
 
   if (game === null) {
